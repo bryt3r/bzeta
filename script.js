a4cf747c8a7f59b7155bd501288ee4e66583883a
bryt3r <direct.tx@gmail.com>
2022-06-11 21:48:35 +0100

    item image selection js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const navLinks = document.querySelector('.nav-links');
 menu.addEventListener('click', () => {
     navLinks.classList.toggle('mobile-nav');
 });
+
+
+function showSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("imageSlide");
+    let thumbs = document.getElementsByClassName("imageThumb");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < thumbs.length; i++) {
+      thumbs[i].className = thumbs[i].className.replace(" activeThumb", "");
+    }
+    slides[n-1].style.display = "block";
+    thumbs[n-1].className += " activeThumb";
+  }
